c62a7de393f6b2b524954169d461ee60af86adfb
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-10-26 15:34:26 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -7,18 +7,18 @@ class Config(object):
     API_ID = "27589257" # integer value, dont use ""
     API_HASH = "0af78b04b48361bc117fa4e06d6d2292"
     TOKEN = "6055798094:AAGWAsG0ZdlyN5pDaxPSGy-7gwuYapRXhBs"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 1137799257 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
     SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
     START_IMG = "https://graph.org/file/396e55a171173d7b24b9b.jpg"
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= "mongodb+srv://drew:drewnotfound@cluster0.haeeyvb.mongodb.net/?retryWrites=true&w=majority"
     # RECOMMENDED
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://csniyeze:yASHXHeIcMxdH6_LCBxw12AvXc--oQxL@suleiman.db.elephantsql.com/csniyeze"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = "YDNAVZ2G1KUL"
     # Get your API key from https://timezonedb.com/api
 
     # Optional fields
